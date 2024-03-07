f65d57ef81c9d14f6a1c5b2310756e11eb6bc91d
himanshuverma13 <fusion.with@skillintelligence.tech>
2024-03-07 15:25:50 +0530

    routes added

---
+++ routesRumeno.py
@@ -0,0 +1,18 @@
+
+def route_methods():
+    route_dict = {
+        "homeRecheck": "/rumeno",
+        "loginRumeno": "/rumeno_login",
+        "signupRumeno": "/rumeno_register",
+        "productDetails": "/product_details",
+        "languageTranslator": "/ln_trans_rumeno",
+        "forgotPwd": "/pwdreset",
+        "checkForgotPwd":"/code_recheck",
+        "subClient": "/subadmin",
+        "getclientdata": "/clientdata",
+        "productCart": "/cart",
+        "transcationDetails": "/transaction_details",
+        "dltcart":"/deleteCart",
+        "logincart":"/cartuser"
+    }
+    return route_dict
